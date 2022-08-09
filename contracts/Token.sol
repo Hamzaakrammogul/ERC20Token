bb9923b67591b9357c6a0f040d93efa5344137df
Hamza Akram <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-08-10 00:43:12 +0500

    first commit

---
+++ contracts/Token.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+abstract contract Token is ERC20, Pausable{
+    address public admin;
+
+ constructor() Pausable() ERC20 ('MyToken', 'HAT') {
+    _mint(msg.sender, 10000*10**18);
+    admin= msg.sender;
+ }
+
+ function mint(address to, uint amount) whenNotPaused external {
+    require(msg.sender==admin,"Only Admin");
+    _mint(to, amount);
+ }
+
+ function burn(uint amount) whenPaused() external{ 
+    _burn(msg.sender, amount);
+ }
+}
+contract RealToken is Token{
+    
+}
