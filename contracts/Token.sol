aaeea25df074efc0997b835f35099ff97d74fd73
Hamza Akram <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-08-10 10:38:39 +0500

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -10,12 +10,12 @@ abstract contract Token is ERC20, Pausable{
     _mint(msg.sender, 10000*10**18);
     admin= msg.sender;
  }
-
+//This function will only work when token is not paused
  function mint(address to, uint amount) whenNotPaused external {
     require(msg.sender==admin,"Only Admin");
     _mint(to, amount);
  }
-
+//This function will only works when token is paused
  function burn(uint amount) whenPaused() external{ 
     _burn(msg.sender, amount);
  }
