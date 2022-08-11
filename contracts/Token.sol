dbece15ca0004e6b996038e6b39942b1b891bad3
Hamza Akram <101792969+Hamzaakrammogul@users.noreply.github.com>
2022-08-11 11:58:59 +0500

    Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -27,7 +27,6 @@ contract Ownable{
         admin = newAdmin;
         newAdmin = address(0);
     }
- 
 }
 
  contract Token is ERC20, Pausable, Ownable{
@@ -36,37 +35,38 @@ contract Ownable{
     _mint(msg.sender, 10000*10**18);
     admin= msg.sender;
  }
+
 //This function will only work when token is not paused
  function mint(address to, uint amount) whenNotPaused external {
     require(msg.sender==admin,"Only Admin");
     _mint(to, amount);
  }
+
 //This function will only works when token is paused
  function burn(uint amount) whenPaused() external{ 
     _burn(msg.sender, amount);
  } 
-   //Transfer tokens when not paused
-     
+
+   //Transfer tokens when not paused 
     function transfer(address to, uint256 amount) public whenNotPaused override returns (bool) {
         return super.transfer(to, amount);
     }
     
    //transferFrom function to tansfer tokens when token is not paused
-     
     function transferFrom(address from, address to, uint256 amount) public whenNotPaused override returns (bool) {
         return super.transferFrom(from, to, amount);
     }
     
-   //approve spender when not paused
-     
+   //approve spender when not paused 
     function approve(address spender, uint256 amount) public whenNotPaused override returns (bool) {
         return super.approve(spender, amount);
     }
-    
+
    //increaseApproval of spender when not paused 
     function increaseAllowance(address spender, uint addedValue) public whenNotPaused override returns (bool ) {
         return super.increaseAllowance(spender, addedValue);
     }
+
    // decreaseApproval of spender when not paused
     function decreaseAllowance(address spender, uint subtractedValue) public whenNotPaused override returns (bool ) {
         return super.decreaseAllowance(spender, subtractedValue);
@@ -76,6 +76,7 @@ contract Ownable{
     function pausedSet() public onlyOwner whenNotPaused  {
       return super._pause();
          }
+
     //This function will unPause the function
     function unPausedSet() public onlyOwner whenPaused  {
         return super._unpause();
